5182c1f83db6e64baff3d1582aeafc262dce2bf5
HanJiazheng <1710121085@qq.com>
2019-01-09 17:15:02 +0800

    增加了显示的细节

--- ui.h
+++ ui.h
@@ -4,7 +4,7 @@
 void OutputHelpMessage()
 {
 	printf("Pigsnose [h][s][l][d][p] [device name] [string]\n");
-	printf("h: show help menu\ns: show more tcp message\nl: show all devices\nd: set specified device to sniff\np: set searched string\n"); 
+	printf("h: show help menu in detail\ns: show more tcp message\nl: show all devices and descriptions\nd: choose specific device to sniff\np: find the string we wish get\n"); 
 }
 void OutputPrompt()
 {
